39ffaa2064befb07ca5dcd5f1de924df1a4e31d3
CiteflaZe <citeflaze@gmail.com>
2019-11-13 02:48:14 +0200

    Reworked access restrictor

--- src/main/java/com/project/filter/AccessRestrictionFilter.java
+++ src/main/java/com/project/filter/AccessRestrictionFilter.java
@@ -13,22 +13,36 @@ public class AccessRestrictionFilter implements Filter {
         final HttpServletRequest request = (HttpServletRequest) servletRequest;
         String adminUrl = request.getServerName() + ":" + request.getServerPort() + "/admin";
         String userUrl = request.getServerName() + ":" + request.getServerPort() + "/user";
+        String homePageUrl = "http://" + request.getServerName() + ":" + request.getServerPort() + "/";
+        String indexUrl = "http://" + request.getServerName() + ":" + request.getServerPort() + "/index.jsp";
+        String command = request.getParameter("command");
         final User user = (User) request.getSession().getAttribute("user");
 
-        if(request.getRequestURL().toString().contains(adminUrl) &&
-                (user == null || user.getRole() != Role.ADMIN)){
-            request.getRequestDispatcher("404-error.jsp").forward(request, servletResponse);
+        if(!"login".equals(command) && !"logout".equals(command) && !"register".equals(command)){
+            if(request.getRequestURL().toString().contains(adminUrl) &&
+                    (user == null || user.getRole() != Role.ADMIN)){
+                request.getRequestDispatcher("404-error.jsp").forward(servletRequest, servletResponse);
+                System.out.println("AdminUrl");
+            }
+            if(request.getRequestURL().toString().contains(userUrl) &&
+                    (user == null || user.getRole() != Role.USER)){
+                request.getRequestDispatcher("404-error.jsp").forward(servletRequest, servletResponse);
+                System.out.println("UserUrl");
+            }
+            if((request.getRequestURL().toString().equals(homePageUrl)||
+                    request.getRequestURL().toString().equals(indexUrl)) &&
+                    user != null && user.getRole() == Role.USER){
+                request.getRequestDispatcher("/user").forward(servletRequest, servletResponse);
+                System.out.println("User");
+            }
+            if((request.getRequestURL().toString().equals(homePageUrl)||
+                    request.getRequestURL().toString().equals(indexUrl)) &&
+                    user != null && user.getRole() == Role.ADMIN){
+                request.getRequestDispatcher("/admin").forward(servletRequest, servletResponse);
+                System.out.println("Admin");
+            }
         }
-        if(request.getRequestURL().toString().contains(userUrl) &&
-                (user == null || user.getRole() != Role.USER)){
-            request.getRequestDispatcher("404-error.jsp").forward(request, servletResponse);
-        }
-        if(user != null && user.getRole() == Role.USER){
-            request.getRequestDispatcher("/user").forward(request, servletResponse);
-        }
-        if(user != null && user.getRole() == Role.ADMIN){
-            request.getRequestDispatcher("/admin").forward(request, servletResponse);
-        }
+
 
         filterChain.doFilter(request, servletResponse);
     }
